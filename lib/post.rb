b653d8eb31c851518de91dce96355b0f40bdc51f
will bailey <will@corp.zenbe.com>
2010-03-12 11:09:47 -0500

    use user from cache

--- lib/post.rb
+++ lib/post.rb
@@ -37,7 +37,7 @@ class Post < Flareshow::Resource
     self.files = []
     self.build_file(file_path)
     self.save
-    self.files = nil
+    self.files = []
   end
   
   # persisted files for this post
@@ -53,7 +53,8 @@ class Post < Flareshow::Resource
   # user for this post
   def user
     return User.current unless user_id
-    User.first({:id => user_id})
+    user = User.get_from_cache(user_id)
+    user || User.first({:id => user_id})
   end
   
   # get the flow for this post
